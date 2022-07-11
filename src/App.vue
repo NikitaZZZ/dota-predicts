cb8677b984ed2f00222a87814a05a88fe15de0c6
Nikita <zininnikita309@gmail.com>
2022-07-11 17:07:23 +0900

    a little bit change styling

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,7 @@
 <template>
-  <div class="container">
-    <h3>Силы света</h3>
-
-    <div class="container heroes mb-3">
-      <div class="row">
-        <heroCard
-          v-for="hero in predictedMatch.selectedHeroesRadiant"
-          :key="hero.heroName"
-          :heroStats="hero"
-        />
-      </div>
-    </div>
-  </div>
-
-  <div class="container mb-3">
-    <h3>Силы тьмы</h3>
-
-    <div class="container heroes mb-3">
-      <div class="row">
-        <heroCard
-          v-for="hero in predictedMatch.selectedHeroesDire"
-          :key="hero.heroName"
-          :heroStats="hero"
-        />
-      </div>
-    </div>
-
-    <button class="btn btn-outline-primary" @click="predict">Посчитать</button>
-
-    <h2>Шансы на победу сил света: {{ Math.floor(predictedMatch.chancesForWinRadiant) }}%</h2>
-    <h2>Шансы на победу сил тьмы: {{ Math.floor(predictedMatch.chancesForWinDire) }}%</h2>
-  </div>
-
-  <select v-model="selectRank.number" @change="getHeroesPercentWins">
-    <option value="0" selected></option>
+  <h1>Текущий выбранный ранг: {{ selectRank.name }}</h1>
+  <select class="form-select mb-3" id="choose-rank" v-show="selectRank.number === 0" v-model="selectRank.number" @change="getHeroesPercentWins">
+    <option value="0" selected>Выберите ранг</option>
     <option value="1">Рекрут</option>
     <option value="2">Страж</option>
     <option value="3">Рыцарь</option>
@@ -44,9 +12,43 @@
     <option value="8">Титан</option>
   </select>
 
-  <h1>Текущий выбранный ранг: {{ selectRank.name }}</h1>
 
-  <div class="search-div input-group mb-3">
+  <div class="pick" v-show="selectRank.number != 0">
+    <div class="container">
+      <h3>Силы света</h3>
+
+      <div class="container heroes mb-3">
+        <div class="row">
+          <heroCard
+            v-for="hero in predictedMatch.selectedHeroesRadiant"
+            :key="hero.heroName"
+            :heroStats="hero"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="container mb-3">
+      <h3>Силы тьмы</h3>
+
+      <div class="container heroes mb-3">
+        <div class="row">
+          <heroCard
+            v-for="hero in predictedMatch.selectedHeroesDire"
+            :key="hero.heroName"
+            :heroStats="hero"
+          />
+        </div>
+      </div>
+
+      <button class="btn btn-outline-primary mb-3" @click="predict">Посчитать</button>
+
+      <h2 v-show="Math.floor(predictedMatch.chancesForWinRadiant) != 0">Шансы на победу сил света: {{ Math.floor(predictedMatch.chancesForWinRadiant) }}%</h2>
+      <h2 v-show="Math.floor(predictedMatch.chancesForWinDire) != 0">Шансы на победу сил тьмы: {{ Math.floor(predictedMatch.chancesForWinDire) }}%</h2>
+    </div>
+  </div>
+
+  <div class="search-div input-group mb-3" v-show="selectRank.number != 0 && Math.floor(predictedMatch.chancesForWinRadiant) === 0">
     <input
       class="form-control"
       type="search"
@@ -58,7 +60,7 @@
     <button class="btn btn-outline-primary" @click="getHeroesPercentWins">Clear</button>
   </div>
 
-  <div class="container heroes mb-3">
+  <div class="container heroes mb-3" v-show="Math.floor(predictedMatch.chancesForWinRadiant) === 0">
     <div class="row">
       <heroCard
         v-for="hero in heroesPercentWins"
@@ -217,6 +219,11 @@ export default {
 
 .search-div {
   width: 300px;
+  margin: auto;
+}
+
+#choose-rank {
+  width: 400px;
   margin: auto;
 }
 </style>
